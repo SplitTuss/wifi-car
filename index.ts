98dcf918259b6d234da049bd39e641c83db37a7b
Lara <lolaviras@gmail.com>
2025-10-27 18:23:30 -0700

    basic blink on keypress demo

--- index.ts
+++ index.ts
@@ -1,2 +1,29 @@
-const x = 7;
-console.log(x);
+import { Board, Led } from 'johnny-five';
+import keypress from 'keypress';
+
+const board = new Board();
+
+board.on('ready', () => {
+  const led = new Led(13);
+
+  keypress(process.stdin);
+
+  process.stdin.on('keypress', (_, key) => {
+    switch (key.name) {
+      case 'up':
+        led.blink(500);
+        break;
+      case 'down':
+        led.strobe(300);
+        break;
+      case 'left':
+        led.fadeOut();
+        break;
+      case 'right':
+        led.blink(200);
+        break;
+      default:
+        console.log('key not recognized');
+    }
+  });
+});
